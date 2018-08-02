eed3697b7580eab702c1f12af96652e4309bef11
Mike Schulenberg <mike.schulenberg@gmail.com>
2018-08-02 22:36:30 +0000

    refactor files to remove unused code

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -44,25 +44,3 @@ $("input[type='text']").keypress(function(event) {
         });
     }
 });
-
-            // `
-            // <li>
-            //     <div class="container-task">
-            //         <div class="task-left-control ${todoPriority}">
-            //             <input type="checkbox">
-            //         </div> 
-            //         <div class="task-body">
-            //             ${todoText}
-            //         </div>
-            //         <div class="task-actions-control">
-            //             <a href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            //                 <i class="fas fa-ellipsis-v" data-toggle="tooltip" data-placement="top" data-boundary="window" title="Actions"></i>
-            //             </a>
-            //             <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-            //                 <a class="dropdown-item" href="/todos/123/edit">Edit</a>
-            //                 <a class="dropdown-item" href="/tasks">Delete</a>
-            //             </div>
-            //         </div>
-            //     </div>
-            // </li>
-            // `
