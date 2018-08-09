d9cd9735f7a458de64eb292c0c6f00b14cc7b9a6
Mike Schulenberg <mike.schulenberg@gmail.com>
2018-08-09 21:52:43 +0000

    add 'new todo' form to main page instead of separate page

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -6,41 +6,25 @@ $(function () {
 });
 
 // Click on checkbox to delete todo
-$("ul").on("click", "input[type=checkbox]", function(event) {
+$("ul").on("click", "input[type=checkbox]", function(e) {
     $(this).closest("li").find(".task-body").toggleClass("completed");
     $(this).closest("li").fadeOut(500, function() {
         $(this).find("#delete-todo-checkbox").submit();
     });
 });
 
-// $("input[type='text']").keypress(function(event) {
-//     // has Enter been pressed?
-//     if (event.which === 13) {
-//         // grab new todo text from input
-//         var todoText = $(this).val();
-//         $(this).val("");
-//         // create a new li and add to ul
-//         var todoPriority = "high-priority"; // temp
-//         $("ul").append(
-//             `
-//             <li>
-//                 <div class="task-left-control ${todoPriority}">
-//                     <input type="checkbox">
-//                 </div>
-//                 <div class="task-body">
-//                     ${todoText}
-//                 </div>
-//                 <a href="/todos/123/edit"><i class="far fa-edit" data-toggle="tooltip" data-placement="top" title="Edit"></i></a>
-//             </li>
-//             `
-//         );
-        
-//         // reinitialize tooltips
-//         $(function () {
-//             $('[data-toggle="tooltip"]').tooltip()
-//         });
-//     }
-// });
+// Unhide the 'new todo' form
+$("#new-todo-input").on("click", function(e) {
+    $(this).siblings("#new-todo-form").removeClass("new-todo-form-hidden");
+    $(this).siblings("#new-todo-form").addClass("new-todo-form-visible");
+});
+
+// Hide the 'new todo' form
+$("#cancel-new-todo").on("click", function(e) {
+    $(this).closest("form").find("#new-todo-form").removeClass("new-todo-form-visible");
+    $(this).closest("form").find("#new-todo-form").addClass("new-todo-form-hidden");
+    $(this).closest("form").find("#new-todo-input").val("");
+});
 
 // Make radio buttons appear as if they've been clicked
 $(".clicked-radio-button").button('toggle')
