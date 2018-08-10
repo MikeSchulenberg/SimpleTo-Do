4bf33f81123c527955192c3a01dfe275e4998d2b
Mike Schulenberg <mike.schulenberg@gmail.com>
2018-08-10 21:41:13 +0000

    add fadeIn when unhiding 'new todo' form and fadeOut when canceling new todo

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -6,7 +6,7 @@ $(function () {
 });
 
 // Click on checkbox to delete todo
-$("ul").on("click", "input[type=checkbox]", function(e) {
+$("ul").on("click", "input[type=checkbox]", function() {
     $(this).closest("li").find(".task-body").toggleClass("completed");
     $(this).closest("li").fadeOut(500, function() {
         $(this).find("#delete-todo-checkbox").submit();
@@ -14,17 +14,34 @@ $("ul").on("click", "input[type=checkbox]", function(e) {
 });
 
 // Unhide the 'new todo' form
-$("#new-todo-input").on("click", function(e) {
-    $(this).siblings("#new-todo-div").removeClass("new-todo-div-hidden");
-    $(this).siblings("#new-todo-div").addClass("new-todo-div-visible");
+$("#new-todo-input").on("click", function() {
+    $(this).siblings("#new-todo-div").fadeIn(500);
+    // $(this).siblings("#new-todo-div").removeClass("new-todo-div-hidden");
+    // $(this).siblings("#new-todo-div").addClass("new-todo-div-visible");
+});
+
+// Unhide the 'new todo' form
+// $("#new-todo-input").on("click", function(e) {
+//     $(this).siblings("#new-todo-div").removeClass("new-todo-div-hidden");
+//     $(this).siblings("#new-todo-div").addClass("new-todo-div-visible");
+// });
+
+// Hide the 'new todo' form
+$("#cancel-new-todo").on("click", function() {
+    $(this).closest("form").find("#new-todo-div").fadeOut(500, function() {
+        $(this).closest("form").find("#new-todo-input").val("");
+    })
+    // $(this).closest("form").find("#new-todo-div").removeClass("new-todo-div-visible");
+    // $(this).closest("form").find("#new-todo-div").addClass("new-todo-div-hidden");
+    // $(this).closest("form").find("#new-todo-input").val("");
 });
 
 // Hide the 'new todo' form
-$("#cancel-new-todo").on("click", function(e) {
-    $(this).closest("form").find("#new-todo-div").removeClass("new-todo-div-visible");
-    $(this).closest("form").find("#new-todo-div").addClass("new-todo-div-hidden");
-    $(this).closest("form").find("#new-todo-input").val("");
-});
+// $("#cancel-new-todo").on("click", function(e) {
+//     $(this).closest("form").find("#new-todo-div").removeClass("new-todo-div-visible");
+//     $(this).closest("form").find("#new-todo-div").addClass("new-todo-div-hidden");
+//     $(this).closest("form").find("#new-todo-input").val("");
+// });
 
 // Make radio buttons appear as if they've been clicked
 $(".clicked-radio-button").button('toggle')
