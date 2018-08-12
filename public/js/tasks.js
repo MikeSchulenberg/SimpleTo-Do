5d0417b2cbabc65c9c39d93a281da50f70a9918f
Mike Schulenberg <mike.schulenberg@gmail.com>
2018-08-12 22:24:38 +0000

    add fadeOut on 'new todo' form when submitting

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -5,7 +5,7 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
 
-// Click on checkbox to delete todo
+// Delete a todo when its checkbox is clicked
 $("ul").on("click", "input[type=checkbox]", function() {
     $(this).closest("li").find(".task-body").toggleClass("completed");
     $(this).closest("li").fadeOut(500, function() {
@@ -13,35 +13,41 @@ $("ul").on("click", "input[type=checkbox]", function() {
     });
 });
 
+// Make radio buttons appear as if they've been clicked
+$(".clicked-radio-button").button('toggle')
+
 // Unhide the 'new todo' form
 $("#new-todo-input").on("click", function() {
     $(this).siblings("#new-todo-div").fadeIn(500);
-    // $(this).siblings("#new-todo-div").removeClass("new-todo-div-hidden");
-    // $(this).siblings("#new-todo-div").addClass("new-todo-div-visible");
 });
-
-// Unhide the 'new todo' form
-// $("#new-todo-input").on("click", function(e) {
-//     $(this).siblings("#new-todo-div").removeClass("new-todo-div-hidden");
-//     $(this).siblings("#new-todo-div").addClass("new-todo-div-visible");
-// });
 
 // Hide the 'new todo' form
 $("#cancel-new-todo").on("click", function() {
     $(this).closest("form").find("#new-todo-div").fadeOut(500, function() {
         $(this).closest("form").find("#new-todo-input").val("");
-    })
-    // $(this).closest("form").find("#new-todo-div").removeClass("new-todo-div-visible");
-    // $(this).closest("form").find("#new-todo-div").addClass("new-todo-div-hidden");
-    // $(this).closest("form").find("#new-todo-input").val("");
+    });
 });
 
-// Hide the 'new todo' form
-// $("#cancel-new-todo").on("click", function(e) {
-//     $(this).closest("form").find("#new-todo-div").removeClass("new-todo-div-visible");
-//     $(this).closest("form").find("#new-todo-div").addClass("new-todo-div-hidden");
-//     $(this).closest("form").find("#new-todo-input").val("");
-// });
+/* Submit a new todo when Enter is pressed in the text input or when the Submit
+   is clicked. ---------------------------------------------------------------*/
 
-// Make radio buttons appear as if they've been clicked
-$(".clicked-radio-button").button('toggle')
+var submitForm = function(thisObj) {
+    thisObj.closest("form").find("#new-todo-input").prop("readonly", true);
+    thisObj.closest("form").find("#new-todo-div").fadeOut(500, function() {
+        thisObj.closest("form").find("#new-todo-input").prop("readonly", false);
+        thisObj.closest("form").submit();
+    });
+};
+
+$("#new-todo-input").on("keypress", function(e) {
+    if (e.which === 13) {
+        e.preventDefault();
+        submitForm($(this));
+    }
+});
+
+$("#submit-new-todo").on("click", function() {
+    submitForm($(this));
+});
+
+//------------------------------------------------------------------------------
