a1a5f605b1a398d071ed2c0116e355fa72b22b78
Mike Schulenberg <mike.schulenberg@gmail.com>
2018-08-21 19:32:04 +0000

    add logic for updating user Tasks Completed and Achievement Point values when completing todos

--- routes/todos.js
+++ routes/todos.js
@@ -1,6 +1,7 @@
 var express = require("express"),
     router  = express.Router(),
-    Todo    = require("../models/todo");
+    Todo    = require("../models/todo"),
+    User    = require("../models/user");
 
 // show form to create a new todo
 router.get("/new", function(req, res) {
@@ -57,6 +58,43 @@ router.put("/:id", function(req, res) {
 
 // destroy todo route
 router.delete("/:id", function(req, res) {
+    /* If the user marked this task as 'complete,' rather than simply deleting
+    it, update the user's info in the database. Increment the number of tasks
+    completed, and award a number of Achievement Points based on the todo's
+    priority. */
+    if (req.body.taskcheckbox) {
+        var completedTasks = req.user.completedTasks + 1;
+        var achievementPoints = req.user.achievementPoints;
+        
+        Todo.findById(req.params.id, function(err, todo) {
+            if (err) {
+                console.log(err);
+            }
+            
+            else {
+                switch (todo.priority) {
+                    case "high":
+                        achievementPoints += 3;
+                        break;
+                    case "medium":
+                        achievementPoints += 2;
+                        break;
+                    case "low":
+                        achievementPoints += 1;
+                        break;
+                }
+                
+                var updatedUser = { completedTasks: completedTasks, achievementPoints: achievementPoints};
+                User.findByIdAndUpdate(req.user._id, updatedUser, function(err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+            }
+        });
+        
+    }
+    
     Todo.findByIdAndRemove(req.params.id, function(err) {
         if (err) {
             console.log(err);
