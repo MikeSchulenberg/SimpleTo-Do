fc0f6eaa61b99272b859bbbf9d351f2ed24df600
Mike Schulenberg <mike.schulenberg@gmail.com>
2018-08-22 22:15:47 +0000

    delete unused routes for showing 'new todo' and 'edit todo' forms as separate pages

--- routes/todos.js
+++ routes/todos.js
@@ -3,11 +3,6 @@ var express = require("express"),
     Todo    = require("../models/todo"),
     User    = require("../models/user");
 
-// show form to create a new todo
-router.get("/new", function(req, res) {
-    res.render("todos/new");
-});
-
 // add new todo to the DB
 router.post("/", function(req, res) {
     var title = req.sanitize(req.body.title);
@@ -25,19 +20,6 @@ router.post("/", function(req, res) {
             res.redirect("/tasks");
         }
     })
-});
-
-// edit todo route - show form to edit a single todo
-router.get("/:id/edit", function(req, res) {
-    Todo.findById(req.params.id, function(err, foundTodo) {
-        if (err) {
-            console.log(err);
-        }
-         
-        else {
-            res.render("todos/edit", {todo: foundTodo});
-        }
-    });
 });
 
 // update todo route - update a single todo in the DB
